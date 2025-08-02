e0bb3513aca97cf491aae866c1cf1fe1c9aef0ee
ImyourShin <bu.tachin_st@tni.ac.th>
2025-08-02 16:00:23 +0700

    TimeModern Clas

---
+++ project1/src/TimeModern.java
@@ -0,0 +1,44 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class TimeModern {
+    private LocalTime time;
+
+    public TimeModern() {
+        this.time = LocalTime.of(0, 0, 0);
+
+    }
+
+    public TimeModern(int hour, int minute, int second) {
+        this.time = LocalTime.of(hour, minute, second);
+    }
+
+    public int getHour() {
+        return time.getHour();
+    }
+
+    public int getMinute() {
+        return time.getMinute();
+    }
+
+    public int getSecond(){
+        return time.getSecond();
+    }
+
+    public TimeModern withTime(int hour , int minute ,int second){
+        return new TimeModern(hour,minute,second);
+    }
+
+
+    public TimeModern nextSecond(){
+        LocalTime newTime = this.time.plusSeconds(1);
+        return new TimeModern(newTime.getMinute(),newTime.getMinute(),newTime.getSecond());
+    }
+
+    @Override
+
+    public String toString(){
+        return time.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+}
